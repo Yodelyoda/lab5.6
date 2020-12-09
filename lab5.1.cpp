bd6df13c0fda8da58e125dc4f491d724408819bf
Yodelyoda <nv.serhiy@gmail.com>
2020-12-09 20:59:01 +0200

    lab5.6.1

---
+++ lab5.1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+double h(const double a, const double b); // прототип
+int main()
+{
+	double s, t;
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+	double c = (h(pow(s,2), 1+t) + h(1,s*t))/(1+pow(h(s,t),2));
+	cout << "c = " << c << endl;
+	return 0;
+}
+double h(const double a, const double b) // визначення
+{
+	return (a+b+pow(a,2)*pow(b,2)) / (pow(a, 2) + pow(b, 2));
+}
